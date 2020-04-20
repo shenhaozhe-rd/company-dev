8ebbd7f625da12e6310cdfb426a9e15f157eceaf
shenhz <1571554543@qq.com>
2020-04-20 16:22:55 +0800

    first commit

---
+++ company-dev/src/main/java/com/companydev/CompanyDevApplication.java
@@ -0,0 +1,13 @@
+package com.companydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyDevApplication.class, args);
+	}
+
+}
---
+++ company-dev/src/test/java/com/companydev/CompanyDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.companydev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
